2dabff22853d92d7e380150616551a3ccb78175c
adarsh3699 <adarsh3699@gmail.com>
2024-09-08 02:34:10 +0530

    Refactor code formatting in .prettierrc.json and GuideMe.js Remove unnecessary empty line in HomePage.js Update font-family and background color in guideMe.css and index.css Add new images and locations in GuideMe.js

--- src/pages/GuideMe.js
+++ src/pages/GuideMe.js
@@ -11,12 +11,15 @@ import ShowMsg from "../components/showMsg/ShowMsg";
 import back_1 from "../images/back_1.jpeg";
 import back_2 from "../images/back_2.jpeg";
 import back_3 from "../images/back_3.jpeg";
-import projector_1 from "../images/projector_1.jpeg";
-import projector_2 from "../images/projector_2.jpeg";
-import projector_3 from "../images/projector_3.jpeg";
 import stage_1 from "../images/stage_1.jpeg";
 import stage_2 from "../images/stage_2.jpeg";
 import stage_3 from "../images/stage_3.jpeg";
+import lion_1 from "../images/lion_1.jpeg";
+import lion_2 from "../images/lion_2.jpeg";
+import lion_3 from "../images/lion_3.jpeg";
+import gate_1 from "../images/gate_1.jpeg";
+import gate_2 from "../images/gate_2.jpeg";
+import gate_3 from "../images/gate_3.jpeg";
 
 import { Slide } from "react-slideshow-image";
 
@@ -36,7 +39,7 @@ const position = [31.2541, 75.7];
 
 const data = [
 	{
-		name: "LPU Block 14 101 L Back Side",
+		name: "LPU Block 14 101L Back Side",
 		latitude: 31.254292175131418,
 		longitude: 75.70565944949598,
 		desc: [
@@ -48,18 +51,7 @@ const data = [
 		img: [back_1, back_2, back_3],
 	},
 	{
-		name: "projector",
-		latitude: 31.25424116115149,
-		longitude: 75.70566477808288,
-		desc: [
-			"1080p projectors offer a resolution of 1920 x 1080 pixels, providing clear and detailed visuals.",
-			"Perfect for home theaters, gaming, and presentations, they match Full HD content like Blu-rays and HD broadcasts.",
-			"There is a emergency exit at the side of the projector.",
-		],
-		img: [projector_1, projector_2, projector_3],
-	},
-	{
-		name: "mainStage",
+		name: "LPU Block 14 101L Stage",
 		latitude: 31.254395777874144,
 		longitude: 75.70563783018873,
 		desc: [
@@ -69,13 +61,34 @@ const data = [
 		],
 		img: [stage_1, stage_2, stage_3],
 	},
+	{
+		name: "LPU Block 14 Lion Statue",
+		latitude: 31.25459689085323,
+		longitude: 75.70554838966639,
+		desc: [
+			"The lion statue is a symbol of strength, courage, and leadership.",
+			"Located in front of Block 14, it is a popular spot for students to take pictures.",
+			"Students often touch the lion's paw for good luck before exams or presentations.",
+		],
+		img: [lion_1, lion_2, lion_3],
+	},
+	{
+		name: "LPU Block 14 101 L Front Gate",
+		latitude: 31.254810290985247,
+		longitude: 75.7056211916548,
+		desc: [
+			"The front gate of Block 14 is the main entrance to the building.",
+			"Students enter and exit through this gate to attend classes, meetings, or events.",
+			"The gate is often decorated with banners, posters, or signs for special occasions.",
+		],
+		img: [gate_1, gate_2, gate_3],
+	},
 ];
 
 function GuideME() {
 	const [loading, setLoading] = useState(true);
-	const [location, setLocation] = useState(null);
-	const [weather, setWeather] = useState(null);
-	const [result, setResult] = useState(null);
+	// const [location, setLocation] = useState(null);
+	// const [weather, setWeather] = useState(null);
 	const [msg, setMsg] = useState({ text: "", type: "" });
 	const [active, setActive] = useState(null);
 
@@ -90,69 +103,54 @@ function GuideME() {
 		}
 	}, []);
 
-	const findMinDistance = (d1, d2, d3) => {
-		if (d1 < d2 && d1 < d3) {
-			return 0;
-		} else if (d2 < d1 && d2 < d3) {
-			return 1;
-		} else {
-			return 2;
+	const findMinDistance = (list) => {
+		let min = list[0];
+		let minIndex = 0;
+		for (let i = 1; i < list.length; i++) {
+			if (list[i] < min) {
+				min = list[i];
+				minIndex = i;
+			}
 		}
+		// console.log("Minimum distance:", min);
+		return minIndex;
+		// console.log("Index of minimum distance:", minIndex);
 	};
+
 	function success(position) {
 		const latitude = position.coords.latitude;
 		const longitude = position.coords.longitude;
-		setLocation({ latitude, longitude });
+		// setLocation({ latitude, longitude });
 		console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 
 		for (let i = 0; i < data.length; i++) {
 			const dis1 = getDistanceFromLatLonInKm(data[0]?.latitude, data[0]?.longitude, latitude, longitude);
 			const dis2 = getDistanceFromLatLonInKm(data[1]?.latitude, data[1]?.longitude, latitude, longitude);
 			const dis3 = getDistanceFromLatLonInKm(data[2]?.latitude, data[2]?.longitude, latitude, longitude);
+			const dis4 = getDistanceFromLatLonInKm(data[3]?.latitude, data[3]?.longitude, latitude, longitude);
+			const dis5 = getDistanceFromLatLonInKm(data[4]?.latitude, data[4]?.longitude, latitude, longitude);
 
-			console.log(dis1, dis2, dis3);
-
-			const distance = findMinDistance(dis1, dis2, dis3);
-			setActive(distance);
-
-			// console.log("distance" + i, distance);
-
-			// if (distance < 6) {
-			// 	setActive(i);
-			// 	// break;
-			// }
+			setActive(findMinDistance([dis1, dis2, dis3, dis4, dis5]));
 		}
 
 		try {
 			fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
 				.then((response) => response.json())
 				.then((data) => {
-					setWeather(data);
+					// setWeather(data);
 					setLoading(false);
-					// handleFetch("about" + data?.address?.amenity);
-					console.log(data);
+					// console.log(data);
 				})
 				.catch((error) => console.log(error));
 		} catch (error) {
 			console.log(error);
+			handleMsgShown("Unable to retrieve your location", "error");
 		}
 	}
 
 	function error1() {
 		console.log("Unable to retrieve your location");
 	}
-
-	// const handleFetch = async (identifier) => {
-	// 	try {
-	// 		// Replace `YOUR_API_ENDPOINT` with the actual Gemini API endpoint
-	// 		const response = await axios.get(`https://api.gemini.com/v1/pubticker/${identifier}`);
-	// 		setResult(response.data);
-	// 		handleMsgShown(null);
-	// 	} catch (err) {
-	// 		// handleMsgShown(err.message);
-	// 		setResult(null);
-	// 	}
-	// };
 
 	function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
 		var R = 6371; // Radius of the earth in km
@@ -170,10 +168,6 @@ function GuideME() {
 		return deg * (Math.PI / 180);
 	}
 
-	// console.log(
-	// 	getDistanceFromLatLonInKm(data[0]?.latitude, data[0]?.longitude, location?.latitude, location?.longitude)
-	// );
-
 	useEffect(() => {
 		function handleLocationClick() {
 			if (navigator.geolocation) {
@@ -184,12 +178,14 @@ function GuideME() {
 			}
 		}
 		handleLocationClick();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	console.log(active);
 
 	return (
 		<div id="GuideME">
+			<div className="GuideME_background"></div>
 			{data?.[active] && (
 				<Slide transitionDuration="700" duration="2000" indicators={true} autoplay={true} easing="ease">
 					{data?.[active]?.img.map((each, index) => (
@@ -202,8 +198,12 @@ function GuideME() {
 
 			<div className="GuideME_box">
 				<Loader isLoading={loading} />
-				<h1>{data?.[active]?.name}</h1>
-				<p>{data?.[active]?.desc}</p>
+				<h1 className="loc_title">{data?.[active]?.name}</h1>
+				<ul>
+					{data?.[active]?.desc?.map((each, index) => (
+						<li key={index}>{each}</li>
+					))}
+				</ul>
 			</div>
 
 			<MapContainer center={position} zoom={13} style={{ height: "50vh", width: "100%" }}>
